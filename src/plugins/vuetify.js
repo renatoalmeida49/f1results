1733f4a57b17bf24e43fc0569d09b2473bb8520e
Renato Novaes <renatoalmeida49@gmail.com>
2022-03-01 14:24:11 -0300

    feat(vuetify.js): language of framework to pt-br

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,24 +1,29 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import pt from 'vuetify/lib/locale/pt'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        dark: true,
-        themes: {
-            light: {
-                red: '#E10600',
-                darkerGray: '#242433',
-                lightenGray: '#454551',
-                white: '#fff',
-            },
-            dark: {
-                red: '#E10600',
-                darkerGray: '#242433',
-                lightenGray: '#454551',
-                white: '#fff',
-            }
-        },
+  theme: {
+    dark: true,
+    themes: {
+      light: {
+        red: '#E10600',
+        darkerGray: '#242433',
+        lightenGray: '#454551',
+        white: '#fff',
+      },
+      dark: {
+        red: '#E10600',
+        darkerGray: '#242433',
+        lightenGray: '#454551',
+        white: '#fff',
+      }
     },
+  },
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  }
 });
